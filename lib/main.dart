36bd8c6f2cd8e49ba7fffeb4568a7570cf735197
yorusicu <jimi008285@gmail.com>
2026-04-26 17:07:49 +0900

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -10,64 +10,56 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    // return MaterialApp(
-    //   title: 'Toy Project',
-    //   theme: ThemeData(
-    //     // This is the theme of your application.
-    //     //
-    //     // TRY THIS: Try running your application with "flutter run". You'll see
-    //     // the application has a purple toolbar. Then, without quitting the app,
-    //     // try changing the seedColor in the colorScheme below to Colors.green
-    //     // and then invoke "hot reload" (save your changes or press the "hot
-    //     // reload" button in a Flutter-supported IDE, or press "r" if you used
-    //     // the command line to start the app).
-    //     //
-    //     // Notice that the counter didn't reset back to zero; the application
-    //     // state is not lost during the reload. To reset the state, use hot
-    //     // restart instead.
-    //     //
-    //     // This works for code too, not just values: Most code changes can be
-    //     // tested with just a hot reload.
-    //     colorScheme: .fromSeed(seedColor: Colors.deepPurple),
-    //   ),
-    //   // home: const MyHomePage(title: 'Flutter Demo Home Page'),
-    //   home: const TestPage(title: 'Toy Project Home Page'),
-    // );
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text('CloneKeep'),
-        centerTitle: true,
+    return MaterialApp(
+      title: 'ClonKeep',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
       ),
-      body: const Center(
-        child: Text('Body Area'),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: () {
-          // 버튼 클릭 시 동작할 내용
-        },
-        child: const Icon(Icons.add),
-      ),
+      home: const TestPage(),
     );
   }
 }
+// return MaterialApp(
+//   title: 'Toy Project',
+//   theme: ThemeData(
+//     // This is the theme of your application.
+//     //
+//     // TRY THIS: Try running your application with "flutter run". You'll see
+//     // the application has a purple toolbar. Then, without quitting the app,
+//     // try changing the seedColor in the colorScheme below to Colors.green
+//     // and then invoke "hot reload" (save your changes or press the "hot
+//     // reload" button in a Flutter-supported IDE, or press "r" if you used
+//     // the command line to start the app).
+//     //
+//     // Notice that the counter didn't reset back to zero; the application
+//     // state is not lost during the reload. To reset the state, use hot
+//     // restart instead.
+//     //
+//     // This works for code too, not just values: Most code changes can be
+//     // tested with just a hot reload.
+//     colorScheme: .fromSeed(seedColor: Colors.deepPurple),
+//   ),
+//   // home: const MyHomePage(title: 'Flutter Demo Home Page'),
+//   home: const MyHomePage(title: 'Flutter Demo Home Page'),
+// );
+//   }
+// }
 
-class TestPage extends StatelessWidget{
+class TestPage extends StatelessWidget {
   const TestPage({super.key});
 
   @override
-  Widget build(BuildContext context){
+  Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: const Text('CloneKeep'),
         centerTitle: true,
+        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
       ),
-      body: const Center(
-        child: Text('Body Area'),
-      ),
+      body: const Center(child: Text('Body Area')),
       floatingActionButton: FloatingActionButton(
-        onPressed: () {
-          // 버튼 클릭 시 동작할 내용
-        },
+        onPressed: () {},
         child: const Icon(Icons.add),
       ),
     );
